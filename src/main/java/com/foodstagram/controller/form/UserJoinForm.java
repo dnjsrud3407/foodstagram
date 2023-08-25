a191bc1c8bfd42d8147a9a7737a755526b936417
dnjsrud3407 <dnjsrud3407@naver.com>
2023-08-25 14:57:20 +0900

    warning으로 emailCheckForm extends 삭제

--- src/main/java/com/foodstagram/controller/form/UserJoinForm.java
+++ src/main/java/com/foodstagram/controller/form/UserJoinForm.java
@@ -1,18 +1,18 @@
 package com.foodstagram.controller.form;
 
 import com.foodstagram.controller.validation.ValidationGroups;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
-@EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserJoinForm extends EmailCheckForm {
+public class UserJoinForm {
 
     @NotBlank(message = "아이디를 입력해주세요.", groups = ValidationGroups.NotBlankGroup.class)
     @Pattern(regexp = "^[a-zA-Z0-9-_]{5,20}$", message = "아이디는 5~20자로 영문 대소문자, 숫자, 특수문자(- 또는 _)만 사용 가능합니다.",
@@ -26,5 +26,15 @@ public class UserJoinForm extends EmailCheckForm {
 
     @NotBlank(message = "비밀번호 확인란에 입력해주세요.", groups = ValidationGroups.NotBlankGroup.class)
     private String passwordConfirm;
+
+    @NotBlank(message = "이메일을 입력해주세요.", groups = ValidationGroups.NotBlankGroup.class)
+    @Email(regexp = "^[a-zA-Z0-9-_]+@[a-z]+\\.[a-z]{2,3}$", message = "이메일 형식이 맞지 않습니다.",
+            groups = ValidationGroups.PatternGroup.class)
+    private String email;
+
+    @NotBlank(message = "인증번호를 입력해주세요.", groups = ValidationGroups.NotBlankGroup.class)
+    @Size(min = 6, max = 6, message = "인증번호 형식이 맞지 않습니다.",
+            groups = ValidationGroups.PatternGroup.class)
+    private String authNum;
 
 }
