89c87ac49e1896662e238ee6d2f56cc8d6fece13
dnjsrud3407 <dnjsrud3407@naver.com>
2023-08-31 13:06:23 +0900

    oracle bucket 사용

--- src/main/java/com/foodstagram/file/FileStore.java
+++ src/main/java/com/foodstagram/file/FileStore.java
@@ -1,16 +1,22 @@
 package com.foodstagram.file;
 
 import com.foodstagram.dto.FoodPictureDto;
+import com.oracle.bmc.ConfigFileReader;
+import com.oracle.bmc.Region;
+import com.oracle.bmc.auth.AuthenticationDetailsProvider;
+import com.oracle.bmc.auth.ConfigFileAuthenticationDetailsProvider;
+import com.oracle.bmc.objectstorage.ObjectStorage;
+import com.oracle.bmc.objectstorage.ObjectStorageClient;
+import com.oracle.bmc.objectstorage.requests.PutObjectRequest;
+import com.oracle.bmc.objectstorage.transfer.UploadConfiguration;
+import com.oracle.bmc.objectstorage.transfer.UploadManager;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 @Component
 public class FileStore {
@@ -31,7 +37,50 @@ public class FileStore {
 
         String originalFilename = multipartFile.getOriginalFilename();
         String storedFileName = createStoreFileName(originalFilename);
-        multipartFile.transferTo(new File(getFullPath(storedFileName)));
+//        multipartFile.transferTo(new File(getFullPath(storedFileName)));
+
+        ConfigFileReader.ConfigFile config =
+                ConfigFileReader.parse("~/.oci/config", "DEFAULT");
+
+        AuthenticationDetailsProvider provider = new ConfigFileAuthenticationDetailsProvider(config);
+
+        ObjectStorage client = new ObjectStorageClient(provider);
+        client.setRegion(Region.AP_CHUNCHEON_1);
+
+        UploadConfiguration uploadConfiguration =
+                UploadConfiguration.builder()
+                        .allowMultipartUploads(true)
+                        .allowParallelUploads(true)
+                        .build();
+
+        UploadManager uploadManager = new UploadManager(client, uploadConfiguration);
+
+        String namespaceName = "axewvmfa4ckn";
+        String bucketName = "foodstagram-bucket";
+        String objectName = storedFileName;
+        Map<String, String> metadata = null;
+        String contentType = "image/jpg";
+
+        String contentEncoding = null;
+        String contentLanguage = null;
+
+        File body = new File("/test.jpg");
+
+        PutObjectRequest request =
+                PutObjectRequest.builder()
+                        .bucketName(bucketName)
+                        .namespaceName(namespaceName)
+                        .objectName(objectName)
+                        .contentType(contentType)
+                        .contentLanguage(contentLanguage)
+                        .contentEncoding(contentEncoding)
+                        .opcMeta(metadata)
+                        .build();
+        UploadManager.UploadRequest uploadDetails =
+                UploadManager.UploadRequest.builder(body).allowOverwrite(true).build(request);
+
+        UploadManager.UploadResponse response = uploadManager.upload(uploadDetails);
+        System.out.println(response);
 
         return new FoodPictureDto(originalFilename, storedFileName, isThumbnail);
     }
