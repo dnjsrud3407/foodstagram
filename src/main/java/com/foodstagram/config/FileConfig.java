5c8927ae1fb655ef244debe5777826fb93200936
dnjsrud3407 <dnjsrud3407@naver.com>
2023-09-01 16:01:28 +0900

    파일 외부경로로 변경

---
+++ src/main/java/com/foodstagram/config/FileConfig.java
@@ -0,0 +1,15 @@
+package com.foodstagram.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class FileConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/picture/**")
+                .addResourceLocations("file:///home/opc/foodstagram/picture/");
+    }
+}
