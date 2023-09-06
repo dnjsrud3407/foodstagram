44ebee37bb74054ac2eb3d0ab8546257f937c204
dnjsrud3407 <dnjsrud3407@naver.com>
2023-09-06 15:43:49 +0900

    list page css 수정

--- src/main/resources/static/js/list.js
+++ src/main/resources/static/js/list.js
@@ -1,17 +1,16 @@
 // 리스트 추가
 function createListForm() {
     // input 태그 추가
-    var htmlString = "<label for='name'>리스트 이름</label>";
-    htmlString += "<input type='text' id='name' required>";
-    htmlString += "<p class='text-error' id='nameErr'></p>";
+    var htmlString = "<label for='name' class='mr1p5r'>리스트 이름</label>";
+    htmlString += "<input type='text' id='name' required class='form-control input-name'>";
+    htmlString += "<p class='text-error inline' id='nameErr'></p>";
 
     document.getElementById('createInput').innerHTML = htmlString;
 
     // 버튼 추가
-    var btnString = "<input type='button' value='등록' onclick='makeList()'>";
-    btnString += "<input type='button' value='취소' onclick='makeListCancel()'>";
+    var btnString = "<input type='button' value='등록' onclick='makeList()' class='btn btn-dark btn-list'>";
+    btnString += "<input type='button' value='취소' onclick='makeListCancel()' class='btn btn-outline-dark btn-list'>";
     document.getElementById('createBtn').innerHTML = btnString;
-
 }
 
 function makeList() {
@@ -38,12 +37,16 @@ function makeList() {
                 document.getElementById('nameErr').innerHTML = data.responseJSON.message;
             }
         });
+    } else {
+        document.getElementById('name').classList.add("fieldError");
+        document.getElementById('nameErr').innerHTML = '리스트 이름을 입력해주세요.';
     }
 }
 
 // 리스트 추가 취소
 function makeListCancel() {
-    var htmlString = "<span style='cursor: pointer;' onclick='createListForm(this)'>+ 새 리스트 만들기</span>";
+    var htmlString = "<img src='/image/plus.png' onclick='createListForm()' style='cursor: pointer; margin-right: 0.3rem'>";
+    htmlString += "<span onclick='createListForm()' style='cursor: pointer;'>새 리스트 만들기</span>";
     document.getElementById('createInput').innerHTML = htmlString;
     document.getElementById('createBtn').innerHTML = "";
 }
@@ -80,10 +83,10 @@ function modifyListForm(listId, listName, totalCnt, obj) {
                 var modifyInputTag = modifyTags[i];
 
                 // input 태그 추가
-                var htmlString = "<input type='text' id='modifyName' value='" + listName + "' required>";
+                var htmlString = "<input type='text' id='modifyName' value='" + listName + "' required class='form-control input-name'>";
                 htmlString += "<input type='hidden' id='originalName' value='" + listName + "'>";
                 htmlString += "<input type='hidden' id='totalCnt' value='" + totalCnt + "'>";
-                htmlString += "<p class='text-error' id='modifyNameErr'></p>";
+                htmlString += "<p class='text-error' id='modifyNameErr inline'></p>";
 
                 modifyInputTag.innerHTML = htmlString;
             }
@@ -91,8 +94,8 @@ function modifyListForm(listId, listName, totalCnt, obj) {
                 var modifyBtnTag = modifyTags[i];
 
                 // 버튼 추가
-                var btnString = "<input type='button' value='수정' onclick='modifyList(" + listId + ")'>";
-                btnString += `<input type='button' value='취소' onclick='modifyListCancel(${listId}, this)'>`;
+                var btnString = "<input type='button' value='수정' onclick='modifyList(" + listId + ")' class='btn btn-dark btn-list'>";
+                btnString += `<input type='button' value='취소' onclick='modifyListCancel(${listId}, this)' class='btn btn-outline-dark btn-list'>`;
                 modifyBtnTag.innerHTML = btnString;
             }
         }
@@ -125,6 +128,9 @@ function modifyList(listId) {
                 }
             }
         });
+    } else {
+        document.getElementById('modifyName').classList.add("fieldError");
+        document.getElementById('modifyNameErr').innerHTML = '리스트 이름을 입력해주세요.';
     }
 }
 
@@ -155,10 +161,10 @@ function modifyListCancel(listId, obj) {
                     btnString += `<span onclick="location.href=&quot;/list/${listId}&quot;" class='link'>상세보기</span>`;
                 }
 
-                btnString += `<span onclick="modifyListForm(${listId}, &quot;${originalName}&quot;, ${totalCnt}, this)" class='link ml0p4r'>수정</span>`;
+                btnString += `<span onclick="modifyListForm(${listId}, &quot;${originalName}&quot;, ${totalCnt}, this)" class='link ml0p8r'>수정</span>`;
 
                 if(totalCnt == 0) {
-                    btnString += `<span onclick='deleteList(${listId})' class='link ml0p4r'>삭제</span>`;
+                    btnString += `<span onclick='deleteList(${listId})' class='link ml0p8r'>삭제</span>`;
                 }
 
                 modifyBtnTag.innerHTML = btnString;
