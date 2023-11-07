261e2cef5bcfa8d41f5e71f82bd72aee72dbd4d2
dnjsrud3407 <dnjsrud3407@naver.com>
2023-11-07 16:17:24 +0900

    filePath 수정

--- src/main/java/com/foodstagram/config/FileConfig.java
+++ src/main/java/com/foodstagram/config/FileConfig.java
@@ -1,5 +1,6 @@
 package com.foodstagram.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,9 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class FileConfig implements WebMvcConfigurer {
 
+    @Value("${file.dir}")
+    private String fileDir;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/picture/**")
-                .addResourceLocations("file:///home/opc/foodstagram/picture/");
+                .addResourceLocations(fileDir);
     }
 }
